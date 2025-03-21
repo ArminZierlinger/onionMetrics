fea7443b0dde4bf421ffd5a09f26313c46779c2b
Armin Zierlinger <armin.zierlinger@gmail.com>
2025-03-21 01:17:36 +0100

    Added entrypoint script.

--- dockerfiles/hiddenMariaDB/install.sh
+++ dockerfiles/hiddenMariaDB/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build -t hidden-mariadb .
-docker run --name hiddenMariadb -d hidden-mariadb tail -f /dev/null
+docker run --name hiddenMariadb -d hidden-mariadb
 docker exec -it hiddenMariadb bash
